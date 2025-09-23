ca4b8e8135d9e29023fc2525c71a9984822dad53
MarshalCypher <abdurasheedkml@gmail.com>
2025-09-23 21:09:46 +0100

    Update server.js

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -7,6 +7,15 @@ app.get("/", function (request, response) {
     response.send("<h1>Hello World</h1>");
 });
 
+app.get("/contact", function (req, res) {
+    res.send("<h1>This is our Contact Page</h1>");
+});
+
+app.get("/about", function(req, res){
+    console.log("I am marshall"); 
+    res.send("<h1>I am marshall</h1>");
+});
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
